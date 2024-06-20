55729f628d996325c33b739d52827fce6f3ab28e
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-06-20 17:32:01 +0200

    1

--- D4.js
+++ D4.js
@@ -131,7 +131,7 @@ console.log(upperFirst("voglio un gelato"));
  della stringa originale.
 */ console.log("esercizio 9");
 const cutString = function (str) {
-  return str.slice(1, str.length - 1);
+  return str.slice(1, str.length - 1); // 1 è la prima e str.legh è la lunghezza meno 1
 };
 console.log(cutString("EPICODE"));
 
@@ -139,11 +139,279 @@ console.log(cutString("EPICODE"));
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
 console.log("esercizio 10");
-const giveMeRandom = function (n) {
-  const arr = [];
-  for (let i = 0; i < n; i++) {
-    arr.push(Math.floor(Math.random() * 10));
+function giveMeRandom(numero) {
+  const myArr = [];
+  for (let i = 0; i < numero; i++) {
+    myArr.push(Math.floor(Math.random() * 10) + 1);
   }
-  return arr;
+  return myArr;
+}
+console.log(giveMeRandom(3));
+// Esercizi aggiuntivi (facoltativi) per D4
+
+/* EXTRA 1
+ Scrivi una funzione chiamata "checkArray" che riceve un array di numeri casuali (creati con la funzione "giveMeRandom") e per ogni elemento stampa in console
+ se il suo valore è maggiore di 5 o no.
+ La funzione deve inoltre ritornare la somma di tutti i valori maggiori di 5.
+*/
+
+console.log("ESERCIZIO EXTRA 1");
+
+const checkArray = function (array) {
+  let summa = 0; // creata per la somma che farò dopo
+  for (let i = 0; i < array.length; i++) {
+    //mi itero tutta la lunghezza dell'array per estrare il numero corrente
+    const currentElement = array[i];
+    if (currentElement > 5) {
+      // se il numero corrente è più grande di 5 allora stampami la somma di tutti i numeri maggiori di 5
+      summa = summa + currentElement;
+      console.log("Il numero", currentElement, "è maggiore di 5"); // stampami i numeri maggiore di 5
+    }
+  }
+  console.log("La somma di tutti i numeri maggiori di 5 è:", summa); // stampami la somma dei numeri
 };
-console.log(giveMeRandom(5));
+
+checkArray(giveMeRandom(5));
+
+/* EXTRA 2
+ Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
+ Crea una funzione chiamata "shoppingCartTotal" che calcola il totale dovuto al negozio (tenendo conto delle quantità di ogni oggetto).
+*/
+
+console.log("ESERCIZIO EXTRA 2");
+
+const shoppingCart = [
+  {
+    price: 10,
+    name: "toilet paper",
+    id: 1,
+    quantity: 6,
+  },
+  {
+    price: 5,
+    name: "teeth brush",
+    id: 2,
+    quantity: 2,
+  },
+  {
+    price: 2,
+    name: "tooth paste",
+    id: 3,
+    quantity: 3,
+  },
+];
+
+const shoppingCartTotal = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    const currentElementPrice = array[i].price;
+    const currentElementQuantity = array[i].quantity;
+    const currentElementTotalPrice =
+      currentElementPrice * currentElementQuantity;
+    sum += currentElementTotalPrice;
+  }
+  console.log("Il totale nel carrello ammonta a:", sum, "euro");
+};
+
+shoppingCartTotal(shoppingCart);
+
+/* EXTRA 3
+ Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
+ Crea una funzione chiamata "addToShoppingCart" che riceve un nuovo oggetto dello stesso tipo, lo aggiunge a "shoppingCart" e ritorna il nuovo numero totale degli elementi.
+*/
+
+console.log("ESERCIZIO EXTRA 3");
+
+const paperBag = {
+  price: 1,
+  name: "paper bag",
+  id: 4,
+  quantity: 1,
+};
+
+const addToShoppingCart = function (obj) {
+  shoppingCart.push(obj);
+  return shoppingCart.length;
+};
+
+const newTotal = addToShoppingCart(paperBag);
+console.log("Il nuovo totale di elementi nel carrello è:", newTotal);
+
+/* EXTRA 4
+ Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
+ Crea una funzione chiamata "maxShoppingCart" che riceve l'array "shoppingCart" e ritorna l'oggetto più costoso in esso contenuto.
+*/
+
+console.log("ESERCIZIO EXTRA 4");
+
+const maxShoppingCart = function (array) {
+  let maxPrice = 0;
+  let maxObj = null;
+  for (let i = 0; i < array.length; i++) {
+    const currentElement = array[i];
+    const currentElementPrice = array[i].price;
+    if (currentElementPrice > maxPrice) {
+      maxObj = currentElement;
+      maxPrice = currentElement;
+    }
+  }
+  return maxObj;
+};
+
+console.log(maxShoppingCart(shoppingCart));
+
+/* EXTRA 5
+ Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
+ Crea una funzione chiamata "latestShoppingCart" che riceve l'array "shoppingCart" e ritorna l'ultimo elemento.
+*/
+
+console.log("ESERCIZIO EXTRA 5");
+
+const latestShoppingCart = function (array) {
+  const lastElementIndex = array.length - 1;
+  const lastElement = array[lastElementIndex];
+  return lastElement;
+};
+
+console.log(latestShoppingCart(shoppingCart));
+
+/* EXTRA 6
+ Crea una funzione chiamata "loopUntil" che riceve un numero intero come parametro con valore tra 0 e 9.
+ La funzione è composta da un ciclo che stampa un numero casuale tra 0 e 9 finchè il numero casuale non è maggiore di x per tre volte di fila.
+*/
+
+console.log("ESERCIZIO EXTRA 6");
+
+const loopUntil = function (int) {
+  if (int > 0 && int < 10 && Number.isSafeInteger(int)) {
+    let randomNumber = 0;
+    let stop = 0;
+    while (randomNumber < int || stop < 3) {
+      const generateRandom = Math.floor(Math.random() * 10);
+      randomNumber = generateRandom;
+      if (randomNumber < int) {
+        console.log(randomNumber);
+      } else {
+        stop++;
+        console.log("stop", stop);
+      }
+    }
+  } else {
+    console.log("Devi inserire un numero intero da 0 a 9!");
+  }
+};
+
+loopUntil(9);
+
+/* EXTRA 7
+Crea una funzione chiamata "average" che riceve un array come parametro e ne ritorna la media aritmetica. La funzione salta automaticamente i valori non numerici nell'array.
+*/
+
+console.log("ESERCIZIO EXTRA 7");
+
+const numArray = [5, 6, 10, "skippalo", 6, "skip"];
+
+const average = function (array) {
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    const currentElement = array[i];
+    if (typeof currentElement === "number") {
+      sum += currentElement;
+      count++;
+    }
+  }
+  const media = sum / count;
+  return media;
+};
+
+console.log(average(numArray));
+
+/* EXTRA 8
+ Crea una funzione chiamata "longest" che trova la stringa più lunga all'interno di un array di stringhe fornito come parametro.
+*/
+
+console.log("ESERCIZIO EXTRA 8");
+
+const stringArray = ["prova", "provalo", "è questa la stringa più lunga?"];
+
+const longest = function (array) {
+  let max = "";
+  for (let i = 0; i < array.length; i++) {
+    const currentElement = array[i];
+    if (currentElement.length > max.length) {
+      max = currentElement;
+    }
+  }
+  console.log(max);
+};
+
+longest(stringArray);
+
+/* EXTRA 9
+ Crea una funzione per creare un filtro anti-spam per la tua casella email. La funzione riceve un parametro stringa chiamato "emailContent", e torna un valore booleano.
+ La funzione deve ritornare true se "emailContent" non contiene le parole "SPAM" o "SCAM".
+*/
+
+console.log("ESERCIZIO EXTRA 9");
+
+const emailContent = "Prova prova prova SCAM";
+const emailContent1 = "prova prova prova";
+
+const antiSpamFilter = function (emailContent) {
+  if (typeof emailContent === "string") {
+    if (!emailContent.includes("SCAM") && !emailContent.includes("SPAM")) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    console.log("Devi inserire una stringa");
+  }
+};
+
+console.log(antiSpamFilter(emailContent));
+console.log(antiSpamFilter(emailContent1));
+
+/* EXTRA 10
+ Scrivi una funzione che riceve una data come parametro, e calcola il numero di giorni passati da quella data.
+*/
+
+console.log("RISPOSTA EXTRA 10");
+
+const date = new Date("1999-5-7");
+
+const fromDate = function (date) {
+  const currentDate = new Date();
+  const dif = currentDate - date;
+  const daysPassed = Math.floor(dif / 86400000);
+  return daysPassed;
+};
+
+console.log("Giorni passati dal quando sono nato:", fromDate(date));
+
+/* EXTRA 11
+ Scrivi una funzione chiamata "matrixGenerator" che riceve come parametri due numeri interi, "x" e "y".
+ Il risultato deve essere una matrice di "x" volte "y", e i valori devono rispecchiare gli indici della posizione all'interno della matrice.
+ Es.: x = 3, y = 2
+ ["00","01","02"
+ "10","11","12"]
+*/
+
+console.log("ESERCIZIO EXTRA 11");
+
+const matrixGenerator = function (x, y) {
+  const matrix = [];
+  for (let b = 0; b < y; b++) {
+    const row = [];
+    for (let i = 0; i < x; i++) {
+      let index = b.toString().concat(i.toString());
+      console.log(index);
+      row.push(index);
+    }
+    matrix.push(row);
+  }
+  console.log(matrix);
+};
+
+matrixGenerator(5, 3);
